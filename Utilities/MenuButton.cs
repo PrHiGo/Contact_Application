dc5fe364c4044b95098c35530234f13b4fdf17d6
Thomas Palmqvist <thomas_palmqvist@live.se>
2023-01-25 17:06:50 +0100

    Made some changes

--- Utilities/MenuButton.cs
+++ Utilities/MenuButton.cs
@@ -1,4 +1,11 @@
 namespace Contact_Application.Utilities
 {
+  public class Toggle {
+    public static bool isOpen;
 
+    public static void OpenMenu()
+    {
+    isOpen = !isOpen;
+    }
+  }
 }
